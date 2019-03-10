8e3bd789d159e30a6d8f9eca6428726ebe7a0a27
ikrotzky <ikr@croworc.com>
2019-03-10 18:42:38 +0100

    refactor: Use two-way data binding for entering/displaying the nickname

--- app/src/main/java/com/croworc/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/croworc/android/aboutme/MainActivity.kt
@@ -6,16 +6,21 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import com.croworc.android.aboutme.data.MyName
 import com.croworc.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName = MyName("Nick van Heussen")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName // Assign the MyName class instance to the binding class's myName variable
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -25,11 +30,16 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(doneButton: View) {
 
         binding.apply {
-            nicknameText.text = nicknameEdit.text       // Set the TextView's text to be the nickname from the EditText
-            invalidateAll()                             // Re-draw the views, so that the new data is shown
-            nicknameEdit.visibility = View.GONE         // Hide the EditText
-            doneButton.visibility = View.GONE           // Hide the DONE button
-            nicknameText.visibility = View.VISIBLE      // Show the nickname TextView
+            myName?.nickname = nicknameEdit.text.toString() /* We're assigning the nickname the user has entered in the
+                                                               EditText to the respective property of our data class
+                                                               instance. As this object is bound to the layout, the
+                                                               nickname will then *automatically* be shown in
+                                                               the TextView.
+                                                            */
+            invalidateAll()                                 // Re-draw the views, so that the new data is shown
+            nicknameEdit.visibility = View.GONE             // Hide the EditText
+            doneButton.visibility = View.GONE               // Hide the DONE button
+            nicknameText.visibility = View.VISIBLE          // Show the nickname TextView
         }
 
         // Hide the keyboard
