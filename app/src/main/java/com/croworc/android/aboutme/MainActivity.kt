89e25c440cfb7c60fa1f7588f44b3789244f1496
ikrotzky <ikr@croworc.com>
2019-03-10 14:05:46 +0100

    feat: Add Click handler for DONE button in onCreate

--- app/src/main/java/com/croworc/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/croworc/android/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.croworc.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+    }
+
+    private fun addNickname(doneButton: View) {
+        // Fetch references to the nickname EditText and the nickname TextView
+        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        // Set the TextView's text to be the nickname from the EditText, then hide the EditText and the button and
+        // show the TextView
+        nicknameTextView.text = nicknameEditText.text
+        nicknameEditText.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
 }
